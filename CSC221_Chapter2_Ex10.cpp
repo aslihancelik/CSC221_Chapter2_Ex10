92f0deb5f4f33d3ce610f6c535f76aa87f69f2b6
aslihancelik <aslihancelik27@gmail.com>
2024-09-26 20:22:58 -0400

    revised dfinished

--- CSC221_Chapter2_Ex10.cpp
+++ CSC221_Chapter2_Ex10.cpp
@@ -1,12 +1,13 @@
 // CSC221_Chapter2_Ex10.cpp : This file contains the 'main' function. This is a program that calculates 
-// the number of miles per gallon the car gets. Program displays the values of current gallons,
-// current miles that can be traveled and the calculated result of miles per gallon.
+// the number of miles per gallon the car gets. The program first asks the user for the input of 
+// number of gallons and the miles driven. Then the program calculates the miles per gallon with 
+// the input. Lastly, program displays the result.
 //
 //
 // Course: CSC 221
 // Programmer: Aslihan Celik
 // Date: 09/26/2024
-// Assignment: Chapter 2 Exercise 10 
+// Assignment: Chapter 2 Exercise 10 Revision
 
 #include <iostream>
 using namespace std;
@@ -14,18 +15,24 @@ using namespace std;
 int main()
 {
 	//The gasoline amount that the car currently holds
-	const double CURRENT_GAS = 15;
+	double gallons;
 	//The miles that the car can travel with the current gasoline amount in its tank
-	const double CURRENT_MILES_TRAVEL = 375;
+	double miles_driven;
 	//Variable to hold the miles per gallon the car can travel
 	double miles_per_gallon;
 
-	//Calculates the number of miles per gallon the car gets
-	miles_per_gallon = CURRENT_MILES_TRAVEL / CURRENT_GAS;
+	//Prompt the user to enter number of gallons
+	cout << "Please enter the number of gallons." << endl;
+	cin >> gallons;
+	cout << "The entered number of gallons is: " << gallons << endl;
 
-	//Display the values used in calculation.
-	cout << "The current number of gallons is " << CURRENT_GAS << "." << endl;
-	cout << "The current miles that can be traveled is " << CURRENT_MILES_TRAVEL << "." << endl;
+	//Prompt the user to enter miles per gallon.
+	cout << "Please enter the miles driven." << endl;
+	cin >> miles_driven;
+	cout << "The entered miles driven is: " << miles_driven << endl;
+
+	//Calculates the number of miles per gallon the car gets
+	miles_per_gallon = miles_driven / gallons;
 
 	//Output the result 
 	cout << "The number of miles per gallon the car gets is " << miles_per_gallon << "." << endl;
